f6a3ef1bc7184279191ac9739a31c142974d552b
Grzegorz Kurek <62963778+gkurek3@users.noreply.github.com>
2024-02-07 15:17:47 +0100

    added HandleLoginPage class (#5)

--- PageObject/Test/Scripts/test_LoginPage.py
+++ PageObject/Test/Scripts/test_LoginPage.py
@@ -1,21 +1,30 @@
-import time
-import unittest
-
 from PageObject.Pages.LoginPage import LTLogin
 from PageObject.Pages.HomePage import LTHomePage
+from PageObject.Pages.HandleLoginPage import HandleLoginPage
 from PageObject.Config.WebdriverSetup import WebdriverSetup
 from PageObject.Config.WebdriverSetupEdge import WebdriverSetupEdge
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-class TestLTLoginPage(unittest.TestCase):
+class TestLTLoginPageClass:
 
-    def setUp(self):
-        super().setUp()
-        self.driver = WebdriverSetupEdge().webdriver_setup()
-        self.lt_home_page_object = LTHomePage(self.driver)
-        self.lt_login_page_object = LTLogin(self.driver)
+    @classmethod
+    def setup_class(cls):
+        print("Setup TestClass!")
+        cls.driver = WebdriverSetup().webdriver_setup()
+        cls.lt_home_page_object = LTHomePage(cls.driver)
+        cls.lt_login_page_object = LTLogin(cls.driver)
+        cls.handle_login_page = HandleLoginPage(cls.driver)
 
-    def test_click_login_link(self):
+    @classmethod
+    def teardown_class(cls):
+        print("Teardown TestClass!")
+        cls.driver.close()
+        cls.driver.quit()
+        del cls.driver
+
+    def test_login_page_loaded(self):
         self.lt_home_page_object.get_lt_login_link().click()
         if self.driver.title == "Log in":
             print("Login page loaded successfully")
@@ -24,22 +33,7 @@ class TestLTLoginPage(unittest.TestCase):
             print("Failed to load")
 
     def test_login_user(self):
-        self.lt_home_page_object.get_lt_login_link().click()
-        self.lt_login_page_object.set_username("gkurek3@gmail.com")
-        self.lt_login_page_object.set_password("Lambda1234")
-        self.lt_login_page_object.click_login_button()
-        time.sleep(2)
-        if self.driver.title == "Welcome - LambdaTest":
-            print("User logged in successfully")
-            assert self.driver.title == "Welcome - LambdaTest"
-        else:
-            print("Failed to log the user in")
-
-    def tearDown(self):
-        self.driver.close()
-        self.driver.quit()
-        del self.driver
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.handle_login_page.handle_login()
+        WebDriverWait(self.driver, 10).until(EC.title_is("Welcome - LambdaTest"))
+        print("User logged in successfully")
+        assert self.driver.title == "Welcome - LambdaTest"
